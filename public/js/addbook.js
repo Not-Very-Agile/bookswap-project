52404b82f9070f2452c956e8fc2a257491baa339
Emily McMullan <eemcmullan92@gmail.com>
2020-11-18 13:23:05 -0500

    added radio buttons

--- public/js/addbook.js
+++ public/js/addbook.js
@@ -2,7 +2,8 @@
 function createBookObject () {
     var bookTitle = document.getElementById("title").value;
     var bookAuthor = document.getElementById("author").value;
-    var bookCondition = document.getElementById("book_condition").value;
+    //var bookCondition = document.getElementById("book_condition").value;
+    var bookCondition = document.querySelector("input[name=book_condition]:checked").value
     var object = {title: bookTitle, author: bookAuthor, book_condition: bookCondition};
     return object;
 }

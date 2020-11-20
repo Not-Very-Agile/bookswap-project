b9f8879f0b28cc614b75f13e487586829df4bc82
forsyth-brian <59516679+forsyth-brian@users.noreply.github.com>
2020-11-20 12:59:36 -0700

    function to add book to db

--- public/js/addbook.js
+++ public/js/addbook.js
@@ -1,25 +1,18 @@
-
-function createBookObject () {
+document.getElementById("addbtn").addEventListener('click', function(event){
+    event.preventDefault();
     var bookTitle = document.getElementById("title").value;
     var bookAuthor = document.getElementById("author").value;
-    //var bookCondition = document.getElementById("book_condition").value;
     var bookCondition = document.querySelector("input[name=book_condition]:checked").value;
     var pointValue = document.getElementById("point_value").value;
     var bookOwner = localStorage.getItem('user');
     var object = {title: bookTitle, author: bookAuthor, book_condition: bookCondition, book_owner: bookOwner, point_value: pointValue};
-    return object;
-};
-
-document.getElementById("addbtn").addEventListener('click', function(event){
-    event.preventDefault();
-    let bookObject = createBookObject();
     const url = "/addbook"
     fetch(url, {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
       },
-      body: JSON.stringify(bookObject)
+      body: JSON.stringify(object)
     })
     window.location.href = "/myshelf";
   });

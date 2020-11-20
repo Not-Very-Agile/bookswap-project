6250a34b2ac1cbbaa157dac96157e19274bf648a
forsyth-brian <59516679+forsyth-brian@users.noreply.github.com>
2020-11-19 18:27:20 -0700

    Updated to take user and point value

--- public/js/addbook.js
+++ public/js/addbook.js
@@ -3,10 +3,12 @@ function createBookObject () {
     var bookTitle = document.getElementById("title").value;
     var bookAuthor = document.getElementById("author").value;
     //var bookCondition = document.getElementById("book_condition").value;
-    var bookCondition = document.querySelector("input[name=book_condition]:checked").value
-    var object = {title: bookTitle, author: bookAuthor, book_condition: bookCondition};
+    var bookCondition = document.querySelector("input[name=book_condition]:checked").value;
+    var pointValue = document.getElementById("point_value").value;
+    var bookOwner = localStorage.getItem('user');
+    var object = {title: bookTitle, author: bookAuthor, book_condition: bookCondition, book_owner: bookOwner, point_value: pointValue};
     return object;
-}
+};
 
 document.getElementById("addbtn").addEventListener('click', function(event){
     event.preventDefault();

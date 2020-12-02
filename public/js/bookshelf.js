616113b7b8e0965bd691f4af80dffca4d14e7a7e
Emily McMullan <eemcmullan92@gmail.com>
2020-12-02 12:30:01 -0500

    Update bookshelf.js

--- public/js/bookshelf.js
+++ public/js/bookshelf.js
@@ -19,17 +19,17 @@ function createBookshelfRows(rowData) {
     var row = $("<tr />")
     $("#bookshelf-table").append(row);
         row.append($("<td id='book_owner' hidden>" + rowData.book_owner + "</td>"));
-        row.append($("<td id='bookid' hidden>" + rowData.bookid + "</td>"));
+        row.append($("<td id='bookId' hidden>" + rowData.bookid + "</td>"));
         row.append($("<td id='book_title'>" + rowData.title + "</td>"));
         row.append($("<td>" + rowData.author + "</td>"));
         row.append($("<td>" + rowData.book_condition + "</td>"));
         row.append($("<td>" + rowData.point_value + "</td>"));
-        row.append($("<td>" + "<button id='swapbtn' onclick='initiateSwap()'>" + "Request Swap" + "</button>" + "</td>"));
+        row.append($("<td>" + "<button class='swapbtn' onclick='initiateSwap()'>" + "Request Swap" + "</button>" + "</td>"));
 }
 
 // Initiate a swap
 function createSwapObject(){
-    var bookID = document.getElementById("bookid").value
+    var bookID = document.getElementById("bookId").value
     var bookOwner = document.getElementById("book_owner").value
     var requestUser = localStorage.getItem('user');
     var object = {reqUser: requestUser, owner: bookOwner, bookid: bookID}
@@ -37,16 +37,19 @@ function createSwapObject(){
   };
   
 function initiateSwap(){
-  document.getElementById("swapbtn").addEventListener('click', function(){
-    let swapObject = createSwapObject();
-    const url = "/addswap"
-    fetch(url, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(swapObject)
-    })
-    window.location.href = "/account";
-  });
+    let swapBtns = document.querySelectorAll(".swapbtn");
+    for (i = 0; i < swapBtns.length; i++){
+        swapBtns[i].addEventListener('click', function(){
+            let swapObject = createSwapObject();
+            const url = "/addswap"
+            fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(swapObject)
+            })
+            window.location.href = "/account";
+        });
+    }
 }
